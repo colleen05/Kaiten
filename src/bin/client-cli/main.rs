3abab7990e81ceabcbd22bb3a88c85cdf75e6be8
Colleen <cdnickerson05@gmail.com>
2023-12-19 00:36:32 -0400

    Implemented client Packet.

--- src/bin/client-cli/main.rs
+++ src/bin/client-cli/main.rs
@@ -1,1 +1,23 @@
-fn main() {}
+use kaiten::{client::Packet, Axis, Move, PlayerColour};
+
+fn main() {
+    let _p_move = Packet::Move(Move {
+        player: PlayerColour::Red,
+        axis: Axis::Row,
+        position: 3,
+        reverse: true,
+        capture: Some((7, 9)),
+    });
+    let _p_move_bytes = _p_move.as_bytes().unwrap();
+    let _p_move_2 = Packet::from_bytes(_p_move_bytes).unwrap();
+
+    let _p_message = Packet::Message(String::from("I am a cool client!"));
+    let _p_message_bytes = _p_message.as_bytes().unwrap();
+    let _p_message_2 = Packet::from_bytes(_p_message_bytes).unwrap();
+
+    println!("{:?}:\n{:?}\n{:?}\n", _p_move, _p_move_2, _p_move_bytes);
+    println!(
+        "{:?}:\n{:?}\n{:?}\n",
+        _p_message, _p_message_2, _p_message_bytes
+    );
+}
